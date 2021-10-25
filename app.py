bb68082fc42e928caa414d227bc7eab1402fb31e
Padmanabha Banerjee <67466151+BlueBlaze6335@users.noreply.github.com>
2021-10-25 16:05:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,98 @@
-from re import DEBUG, sub
-from flask import Flask, render_template, request, redirect, send_file, url_for
-from werkzeug.utils import secure_filename, send_from_directory
+import streamlit as st
+from detect import main
 import os
-import subprocess
+import base64
+from PIL import Image
 
-app = Flask(__name__)
-
-uploads_dir = os.path.join(app.instance_path, 'uploads')
-
-os.makedirs(uploads_dir, exist_ok=True)
-
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+source_dict = {
+    "Camera": False,
+    "Uploaded Video": True
+}
 
 
-@app.route("/detect", methods=['POST'])
-def detect():
-    if not request.method == "POST":
-        return
-    video = request.files['video']
-    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
-    print(video)
-    subprocess.run("ls")
-    subprocess.run(['python3', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))])
+def app():
+    background()
+    thresh, source = sidebar()
+    st.markdown("<h3 style='text-align: center; color:#99ffff;'>Realtime Crowd Monitoring System</h3>",
+                unsafe_allow_html=True)
+    if source_dict[source]:
+        uploaded_file = st.file_uploader('Upload the required video file', type=['mp4', 'mkv'])
+    if st.sidebar.button('SUBMIT'):
+        if source_dict[source]:
+            if uploaded_file is not None:
+                st.markdown("<h5 style='text-align: center; color:#99ffff;'>Data Loaded...</h5>",
+                            unsafe_allow_html=True)
+                with open(os.path.join(os.getcwd(), uploaded_file.name), "wb") as f:
+                    f.write(uploaded_file.getbuffer())
+                main(source = os.path.join(os.getcwd(), uploaded_file.name), conf_thresh = thresh)
+                os.remove(os.path.join(os.getcwd(), uploaded_file.name))
+            else:
+                st.markdown("<h2 style='text-align: center; color:#99ffff;'>Upload the video to detect.</h2>",
+                        unsafe_allow_html=True)
+        else:
+            main(source="0", conf_thresh=thresh)
 
-    # return os.path.join(uploads_dir, secure_filename(video.filename))
-    obj = secure_filename(video.filename)
-    return obj
 
-@app.route('/return-files', methods=['GET'])
-def return_file():
-    obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
-    print(loc)
-    try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
-        # return send_from_directory(loc, obj)
-    except Exception as e:
-        return str(e)
+def sidebar():
+    side_bg = os.path.join(os.getcwd(), 'static/logo.jpg')
+    sidebar_logo = Image.open(side_bg)
+    st.sidebar.image(sidebar_logo)
+    st.sidebar.markdown("""# Customize View""")
+    thresh = user_inputs()
+    return thresh
 
-# @app.route('/display/<filename>')
-# def display_video(filename):
-# 	#print('display_video filename: ' + filename)
-# 	return redirect(url_for('static/video_1.mp4', code=200))
+
+def background():
+    main_bg = os.path.join(os.getcwd(),'static/bg.jpg')
+    main_bg_ext = "jpg"
+    st.markdown(
+        f"""
+        <style>
+        .reportview-container {{
+            background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+        }}
+         background-position: center;
+      background-repeat: no-repeat;
+    background-size: cover;
+    }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    banner()
+
+
+def banner():
+    col1, col2, col3 = st.columns([1, 6, 1])
+
+    with col1:
+        st.write("")
+
+    with col2:
+        image = Image.open('static/banner.png')
+        st.image(image, width=500)
+
+    with col3:
+        st.write("")
+
+def buttons():
+    col1, col2, col3 = st.columns([13,6,10])
+
+    with col1:
+        st.write("")
+
+    with col2:
+        stop = st.button('STOP')
+
+    with col3:
+        st.write("")
+
+    return stop
+
+def user_inputs():
+    thresh = st.sidebar.slider('Threshold', 0.0, 1.0, 0.35)
+    source = st.sidebar.selectbox("Media Source", ("Camera","Uploaded Video"))
+    return (thresh,source)
+
+if __name__ == '__main__':
+    app()
