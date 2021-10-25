0f0a46254168fce3d3d4ee64b271c6bffa337024
Padmanabha Banerjee <67466151+BlueBlaze6335@users.noreply.github.com>
2021-10-25 16:06:01 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,9 +1,3 @@
-"""Run inference with a YOLOv5 model on images, videos, directories, streams
-
-Usage:
-    $ python path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
-"""
-
 import argparse
 import sys
 import time
@@ -14,7 +8,7 @@ import torch
 import torch.backends.cudnn as cudnn
 
 FILE = Path(__file__).absolute()
-sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
+sys.path.append(FILE.parents[0].as_posix())
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
@@ -25,18 +19,18 @@ from utils.torch_utils import select_device, load_classifier, time_sync
 import streamlit as st
 
 @torch.no_grad()
-def run(weights='yolov5s.pt',  # model.pt path(s)
-        source='data/images',  # file/dir/URL/glob, 0 for webcam
-        imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
+def run(source,  # file/dir/URL/glob, 0 for webcam
+        weights='crowd_best.pt',  # model.pt path(s)q
+        imgsz=416,  # inference size (pixels)
+        conf_thresh=0.3,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
+        view_img=True,  # show results
         save_txt=False,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
+        nosave=True,  # do not save images/videos
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
@@ -45,10 +39,9 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         project='static',  # save results to project/name
         name='',  # save results to project/name
         exist_ok=True,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
+        line_thickness=2,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
         hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
         ):
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
@@ -61,25 +54,17 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     # Initialize
     set_logging()
     device = select_device(device)
-    half &= device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
     w = weights[0] if isinstance(weights, list) else weights
-    classify, pt, onnx = False, w.endswith('.pt'), w.endswith('.onnx')  # inference type
+    pt = True
     stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
     if pt:
         model = attempt_load(weights, map_location=device)  # load FP32 model
         stride = int(model.stride.max())  # model stride
         names = model.module.names if hasattr(model, 'module') else model.names  # get class names
-        if half:
-            model.half()  # to FP16
-        if classify:  # second-stage classifier
-            modelc = load_classifier(name='resnet50', n=2)  # initialize
-            modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
-    elif onnx:
-        check_requirements(('onnx', 'onnxruntime'))
-        import onnxruntime
-        session = onnxruntime.InferenceSession(w, None)
+    st.markdown("<h3 style='text-align: center; color:#99ffff;'>Model Loaded...</h3>",
+                unsafe_allow_html=True)
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Dataloader
@@ -100,31 +85,24 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     for path, img, im0s, vid_cap in dataset:
         if pt:
             img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
-        elif onnx:
-            img = img.astype('float32')
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            img = img.float()
+        img /= 255.0
         if len(img.shape) == 3:
-            img = img[None]  # expand for batch dim
+            img = img[None]
 
         # Inference
         t1 = time_sync()
         if pt:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(img, augment=augment, visualize=visualize)[0]
-        elif onnx:
-            pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
 
         # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        pred = non_max_suppression(pred, conf_thresh, iou_thres, classes, agnostic_nms, max_det=max_det)
         t2 = time_sync()
 
-        # Second-stage classifier (optional)
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
-
         # Process predictions
-        for i, det in enumerate(pred):  # detections per image
+        countCrowd = 0
+        for i, det in enumerate(pred):# detections per image
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
             else:
@@ -155,6 +133,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
+                        #cprint(f"{c,names[c]}",'blue')
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=line_thickness)
                         if save_crop:
@@ -191,58 +170,10 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {colorstr('bold', save_dir)}{s}")
 
-    if update:
-        strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
-
     print(f'Done. ({time.time() - t0:.3f}s)')
 
     return save_path
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='crowd_best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.45, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.50, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='static', help='save results to project/name')
-    parser.add_argument('--name', default='', help='save results to project/name')
-    parser.add_argument('--exist-ok', default=True, action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    opt = parser.parse_args()
-    return opt
-
-
-def main(opt):
-    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
+def main(source, conf_thresh = 0.3):
     check_requirements(exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
-    print(path)
-    vs = cv2.VideoCapture(path)  # 1 millisecond
-    stframe = st.empty()
-    while vs.isOpened():
-        ret, frame = vs.read()
-        try:
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            stframe.image(rgb)
-        except:
-            pass
-
-if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+    run(source = source, conf_thresh=conf_thresh)
